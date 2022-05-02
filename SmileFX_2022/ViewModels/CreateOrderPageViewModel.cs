f8f16513724e3f6c9353e4b28310bfd6784dd65d
manzotta <42544578+manzotta@users.noreply.github.com>
2022-05-02 23:35:27 +0200

    Major update v6

--- SmileFX_2022/ViewModels/CreateOrderPageViewModel.cs
+++ SmileFX_2022/ViewModels/CreateOrderPageViewModel.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
