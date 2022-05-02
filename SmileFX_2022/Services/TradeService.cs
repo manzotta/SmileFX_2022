444b68d14b0fe99725e1b9691149eb1a531129f8
manzotta <42544578+manzotta@users.noreply.github.com>
2022-05-02 20:59:03 +0200

    Create TradeService.cs

---
+++ SmileFX_2022/Services/TradeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmileFX_2022.Services
+{
+    internal class TradeService
+    {
+    }
+}
